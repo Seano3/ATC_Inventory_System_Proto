8d8f97502254b90eb847bbebf73e9218a331f9fa
Sean Thornton <sthornton8@outlook.com>
2024-03-21 11:28:56 -0400

    Table looks nicer

--- inventory/src/pages/Public/PublicTable.js
+++ inventory/src/pages/Public/PublicTable.js
@@ -1,5 +1,6 @@
 import { getDatabase } from '../../Database/DatabaseInteractions';
 import React, { useState, useEffect } from 'react';
+import "./CSS/PublicTable.css"
 
 const PublicTable = () => {
 	const [data, setData] = useState([]);
@@ -9,34 +10,36 @@ const PublicTable = () => {
 	}, []);
 
 	return (
-		<table>
-			<thead>
-				<tr>
-					<th>ID</th>
-					<th>Type</th>
-					<th>Battery Type</th>
-					<th>Battery Date</th>
-					<th>Location</th>
-					<th>Origination Date</th>
-					<th>Seral Number</th>
-					<th>Notes</th>
-				</tr>
-			</thead>
-			<tbody>
-				{data.map(item => (
-					<tr key={item.ID}>
-						<td>{item.ID}</td>
-						<td>{item.Type}</td>
-						<td>{item.BatteryType}</td>
-						<td>{item.BatteryDate}</td>
-						<td>{item.Location}</td>
-						<td>{item.OriginationDate}</td>
-						<td>{item.SeralNumber}</td>
-						<td>{item.Notes}</td>
+		<html>
+			<table id = "public">
+				<thead>
+					<tr>
+						<th>ID</th>
+						<th>Type</th>
+						<th>Battery Type</th>
+						<th>Battery Date</th>
+						<th>Location</th>
+						<th>Origination Date</th>
+						<th>Seral Number</th>
+						<th>Notes</th>
 					</tr>
-				))}
-			</tbody>
-		</table>
+				</thead>
+				<tbody>
+					{data.map(item => (
+						<tr key={item.ID}>
+							<td>{item.ID}</td>
+							<td>{item.Type}</td>
+							<td>{item.BatteryType}</td>
+							<td>{item.BatteryDate}</td>
+							<td>{item.Location}</td>
+							<td>{item.OriginationDate}</td>
+							<td>{item.SeralNumber}</td>
+							<td>{item.Notes}</td>
+						</tr>
+					))}
+				</tbody>
+			</table>
+		</html>
 	);
 };
 
