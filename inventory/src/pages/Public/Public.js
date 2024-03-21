8a667f2d6bf0837d05d4676a98a26c1674b19b99
Sean Thornton <sthornton8@outlook.com>
2024-03-21 10:02:03 -0400

    Started css

--- inventory/src/pages/Public/Public.js
+++ inventory/src/pages/Public/Public.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { updateLocation } from '../../Database/DatabaseInteractions';
+import './CSS/Public.css'
 
 
 const Public = () => {
@@ -9,14 +10,14 @@ const Public = () => {
 
     const handleATChange = (event) => {
         setATID(event.target.value);
-     };
+    };
 
     const handleLocoChange = (event) => {
         setLoco(event.target.value);
     };
 
     const update = () => {
-        updateLocation(ATID, Loco); 
+        updateLocation(ATID, Loco);
     }
 
     useEffect(() => {
@@ -26,26 +27,28 @@ const Public = () => {
         if (ATIDParam) {
             setATID(ATIDParam);
         }
-     }, []);
+    }, []);
 
-    return(    
+    return (
         <div>
-            <h1>Public</h1>
-            <label>Enter AT ID: </label>
-            <input
-                type="text"
-                value={ATID} // Step 3: Set the value of the input to the state variable
-                onChange={handleATChange} // Step 4: Update the state variable on input change
-            />
-            <label>Enter New Location: </label>
-            <input
-                type="text"
-                value={Loco} // Step 3: Set the value of the input to the state variable
-                onChange={handleLocoChange} // Step 4: Update the state variable on input change
-            />
-            <button onClick={update()}>
-                Update
-            </button>
+            <body>
+                <h1>Public</h1>
+                <label>Enter AT ID: </label>
+                <input
+                    type="text"
+                    value={ATID} // Step 3: Set the value of the input to the state variable
+                    onChange={handleATChange} // Step 4: Update the state variable on input change
+                />
+                <label>Enter New Location: </label>
+                <input
+                    type="text"
+                    value={Loco} // Step 3: Set the value of the input to the state variable
+                    onChange={handleLocoChange} // Step 4: Update the state variable on input change
+                />
+                <button onClick={update()}>
+                    Update
+                </button>
+            </body>
         </div>
     );
 };
