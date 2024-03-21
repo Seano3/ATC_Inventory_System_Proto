a5bbd7dde80413f916c37c57c5a649a0634db88d
Sean Thornton <sthornton8@outlook.com>
2024-03-21 10:10:29 -0400

    Added body to admin

--- inventory/src/pages/Admin/Admin.js
+++ inventory/src/pages/Admin/Admin.js
@@ -3,16 +3,18 @@ import { Outlet, Link } from "react-router-dom";
 const Admin = () => {
     return (
         <div>
-            <h1>Admin</h1>
-            <ul>
-                <li>
-                    <Link to="/Admin/Table">Table</Link>
-                </li>
-                <li>
-                    <Link to="/Admin/EditAT">EditAT</Link>
-                </li>
-            </ul>
-            <Outlet />
+            <body>
+                <h1>Admin</h1>
+                <ul>
+                    <li>
+                        <Link to="/Admin/Table">Table</Link>
+                    </li>
+                    <li>
+                        <Link to="/Admin/EditAT">EditAT</Link>
+                    </li>
+                </ul>
+                <Outlet />
+            </body>
         </div>
 
     );
