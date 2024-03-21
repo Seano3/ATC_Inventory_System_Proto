2793b96eaf20e24497a704e1bf97f237c4953355
Sean <spthornton@wpi.edu>
2024-03-21 18:51:07 -0400

    Links begging

--- inventory/src/pages/Admin/Table.js
+++ inventory/src/pages/Admin/Table.js
@@ -1,11 +1,51 @@
+import { getDatabase } from '../../Database/DatabaseInteractions';
+import React, { useState, useEffect } from 'react';
+import { Outlet, Link } from "react-router-dom";
 import "./CSS/Table.css"
 
 const Table = () => {
-    return (
-        <div>
-            <h1>Table</h1>
-        </div>
-    );
+        const [data, setData] = useState([]);
+
+	useEffect(() => {
+		getDatabase().then(fetchedData => setData(fetchedData));
+	}, []);
+
+	return (
+		<html>
+			<table id = "table">
+				<thead>
+					<tr>
+						<th>ID</th>
+						<th>Type</th>
+						<th>Battery Type</th>
+						<th>Battery Date</th>
+						<th>Location</th>
+						<th>Origination Date</th>
+						<th>Serial Number</th>
+						<th>Make</th>
+						<th>Model</th>
+						<th>Notes</th>
+					</tr>
+				</thead>
+				<tbody>
+					{data.map(item => (
+						<tr key={item.ID}>
+							<td><Link to="/Admin/EditAT/?param=">{item.ID}</Link></td>
+							<td>{item.Type}</td>
+							<td>{item.BatteryType}</td>
+							<td>{item.BatteryDate}</td>
+							<td>{item.Location}</td>
+							<td>{item.OriginationDate}</td>
+							<td>{item.SeralNumber}</td>
+							<td>{item.Make}</td>
+							<td>{item.Model}</td>
+							<td>{item.Notes}</td>
+						</tr>
+					))}
+				</tbody>
+			</table>
+		</html>
+	);
 };
 
 export default Table; 
