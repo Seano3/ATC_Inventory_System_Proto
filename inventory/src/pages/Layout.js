a948758f0d1c01c7ba445ed4a0945f1b566eba23
Sean Thornton <sthornton8@outlook.com>
2024-03-25 13:16:15 -0400

    added id to nav

--- inventory/src/pages/Layout.js
+++ inventory/src/pages/Layout.js
@@ -4,16 +4,15 @@ import "./CSS/Layout.css";
 const Layout = () => {
     return (
         <>
-            <nav>
+            <nav class="topNav">
                 <ul>
-                    
-                        <h1>DDS ATC Inventory System
+                    <h1>DDS ATC Inventory System
                         {/* <Link to="/"><img align="Left" src="https://www.mass.gov/libraries/mayflower-artifacts/assets/images/logo/stateseal.png" width="20" height="20"></img></Link> */}
                         <Link to="/"><button>Public</button></Link>
                         <Link to="/Admin"><button>Admin</button></Link>
                         <Link to="/PublicTable"><button>Table</button></Link>
-                        </h1>
-                    
+                    </h1>
+
                 </ul>
             </nav>
             <Outlet />
