d945be56924402cd1e3f10b070c118f2d3fada49
Sean Thornton <sthornton8@outlook.com>
2024-03-25 13:13:28 -0400

    Moved h1

--- inventory/src/pages/Layout.js
+++ inventory/src/pages/Layout.js
@@ -6,13 +6,14 @@ const Layout = () => {
         <>
             <nav>
                 <ul>
-                    <header className="NavBar">
-                        <h1>DDS ATC Inventory System</h1>
+                    
+                        <h1>DDS ATC Inventory System
                         {/* <Link to="/"><img align="Left" src="https://www.mass.gov/libraries/mayflower-artifacts/assets/images/logo/stateseal.png" width="20" height="20"></img></Link> */}
                         <Link to="/"><button>Public</button></Link>
                         <Link to="/Admin"><button>Admin</button></Link>
                         <Link to="/PublicTable"><button>Table</button></Link>
-                    </header>
+                        </h1>
+                    
                 </ul>
             </nav>
             <Outlet />
