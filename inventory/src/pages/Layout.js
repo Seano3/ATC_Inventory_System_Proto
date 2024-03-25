53807681072bbaf42a60f0b6328b61198b9dfeda
Sean Thornton <sthornton8@outlook.com>
2024-03-25 12:20:52 -0400

    Added Mass Logo

--- inventory/src/pages/Layout.js
+++ inventory/src/pages/Layout.js
@@ -8,7 +8,7 @@ const Layout = () => {
             <nav>
                 <ul>
                     <header className="NavBar">
-                        <Link to="/"><img align="right" src="https://www.nicepng.com/png/full/17-178841_home-png-home-icon-free.png" width="20" height="20"></img></Link>
+                        <Link to="/"><img align="Left" src="https://www.mass.gov/libraries/mayflower-artifacts/assets/images/logo/stateseal.png" width="20" height="20"></img></Link>
                         <Link to="/"><button>Public</button></Link>
                         <Link to="/Admin"><button>Admin</button></Link>
                         <Link to="/PublicTable"><button>Table</button></Link>
