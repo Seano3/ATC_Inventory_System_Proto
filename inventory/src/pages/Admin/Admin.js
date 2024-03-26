8780178a3695e3b3fde9bac8c2e5bb7a60538190
Sean Thornton <sthornton8@outlook.com>
2024-03-26 10:02:21 -0400

    Added Admin Title Bar

--- inventory/src/pages/Admin/Admin.js
+++ inventory/src/pages/Admin/Admin.js
@@ -4,19 +4,19 @@ import "./CSS/Admin.css"
 const Admin = () => {
     return (
         <div>
-            <body>
-                <h1>Admin</h1>
-                <ul>
-                    <li>
-                        <Link to="/Admin/Table">Table</Link>
-                    </li>
-                    <li>
-                        <Link to="/Admin/EditAT">EditAT</Link>
-                    </li>
-                </ul>
-                <Outlet />
-            </body>
-        </div>
+            <nav class="topNav">
+                <header>
+                    <ul>
+                        <h1>Admin
+                            <Link to="/Admin/Table" class="navButtons"><a>Table</a></Link>
+                            <Link to="/Admin/EditAT" class="navButtons"><a>EditAT</a></Link>
+                        </h1>
+
+                    </ul>
+                </header>
+            </nav>
+            <Outlet />
+        </div >
 
     );
 };
