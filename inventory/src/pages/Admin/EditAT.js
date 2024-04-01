bde51c05b50210d9cce01287ee7e94b24e6a664b
Sean Thornton <sthornton8@outlook.com>
2024-04-01 09:15:06 -0400

    EditAT Updates

--- inventory/src/pages/Admin/EditAT.js
+++ inventory/src/pages/Admin/EditAT.js
@@ -1,21 +1,81 @@
 import "./CSS/EditAT.css"
-import React, { useEffect } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
+import { getAT } from '../../Database/DatabaseInteractions';
+import QRCode from "react-qr-code";
+import * as htmlToImage from 'html-to-image';
 
 
 const EditAT = () => {
+    const qrCodeRef = useRef(null);
+
+    const [ATID, setATID] = useState('');
+    const [ATtype, setATType] = useState('');
+    const [powerType, setPowerType] = useState('');
+    const [batteryType, setBatteryType] = useState('');
+    const [batteryDate, setBatteryDate] = useState('');
+    const [location, setLocation] = useState('');
+    const [originationDate, setOriginationDate] = useState('');
+    const [serialNumber, setSerialNumber] = useState('');
+    const [make, setMake] = useState('');
+    const [model, setModel] = useState('');
+    const [notes, setNotes] = useState('');
+
+    const handleATIDChange = (event) => {
+        setATID(event.target.value);
+    };
+
+    const handleATTypeChange = (event) => {
+        setATType(event.target.value);
+    }
 
     useEffect(() => {
         const urlParams = new URLSearchParams(window.location.search);
-        const ATIDParam = urlParams.get('ATIDParam'); 
-        console.log(ATIDParam);
-        if (ATIDParam) {
-            
+        const param = urlParams.get('param');
+        console.log(param);
+        if (param) {
+            let item = getAT(param);
+            setATID(param);
+            // setATType(item.ATtype);
+            // setPowerType(item.Type);
+            // setBatteryType(item.BatteryType);
+            // setBatteryDate(item.BatteryDate);
+            // setLocation(item.Location);
+            // setOriginationDate(item.OriginationDate);
+            // setSerialNumber(item.SeralNumber);
+            // setMake(item.Make);
+            // setModel(item.Model);
+            // setNotes(item.Notes);
         }
     }, []);
 
+    const downloadQRCode = () => {
+        htmlToImage.toPng(qrCodeRef.current)
+            .then((dataUrl) => {
+                const link = document.createElement('a');
+                link.href = dataUrl;
+                link.download = 'qr-code.png';
+                link.click();
+            })
+            .catch((error) => {
+                console.error('Error generating QR code:', error);
+            });
+    };
 
-    return(
-        <h1>EditAT</h1>
+
+    return (
+        <div>
+            <h1>EditAT</h1>
+            <label>Enter AT ID: </label>
+            <input
+                type="text"
+                value={ATID} // Step 3: Set the value of the input to the state variable
+                onChange={handleATIDChange} // Step 4: Update the state variable on input change
+            />
+            <div ref={qrCodeRef} id="QRCode">
+                <QRCode value={`https://youthful-butterfly.static.domains/?ATIDParam=${ATID}`} />
+            </div>
+            <button onClick={downloadQRCode}>Download QR Code</button>
+        </div>
     );
 };
 
